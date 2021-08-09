d42059d3d432d1682205d31b8a982a66110ac1fe
rimgo <grigoryang085@gmail.com>
2021-08-09 04:09:09 +0300

    feat: add join emit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { FormEventHandler, useEffect, useRef, useState } from "react";
 import io, { Socket } from "socket.io-client";
 
+type TClient = {
+    id: string;
+    name: string;
+};
+
 function App() {
-    const [clientCount, setClientCount] = useState<number>(0);
+    const [name, setName] = useState<string>("");
+    const [clients, setClients] = useState<TClient[]>([]);
     const socketRef = useRef<Socket>();
+
+    const submitHandler: FormEventHandler = (event) => {
+        event.preventDefault();
+        name && socketRef.current?.emit("join", name);
+    };
 
     useEffect(() => {
         socketRef.current = io("http://192.168.1.7:3003/");
-        console.log(socketRef);
 
-        socketRef.current?.on("client-count", (message: { data: number }) => {
-            setClientCount(message.data);
+        socketRef.current?.on("get-clients", (message: { data: TClient[] }) => {
+            setClients(message.data);
         });
     }, []);
 
-    return <div className="App">Clients {clientCount}</div>;
+    return (
+        <div className="App">
+            <form onSubmit={submitHandler}>
+                <input value={name} onChange={(event) => setName(event.target.value)} type="text" />
+                <button type="submit">Join</button>
+            </form>
+            <ul>
+                {clients.map((client) => (
+                    <li key={client.id}>{client.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
